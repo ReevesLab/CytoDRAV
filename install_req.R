4cd321722bba1f3d62166b09c01eb570502a3267
Kyle Kroll <kkroll1@bidmc.harvard.edu>
2019-05-02 16:01:01 -0400

    Removed hard call to robustbase install

--- install_req.R
+++ install_req.R
@@ -2,7 +2,6 @@ install.packages(c("shiny", "promises", "future",
                    "dplyr", "stringr", "colourpicker", 
                    "Rtsne", "ggplot2"),
                  repos='http://cran.us.r-project.org')
-install.packages("https://cran.r-project.org/bin/macosx/el-capitan/contrib/3.6/robustbase_0.93-4.tgz", repos=NULL)
 install.packages("https://cran.r-project.org/src/contrib/Archive/mvtnorm/mvtnorm_1.0-8.tar.gz", repos=NULL)
 source("https://bioconductor.org/biocLite.R")
 BiocInstaller::biocLite("flowCore")
