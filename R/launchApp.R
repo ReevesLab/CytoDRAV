940d25b2a0d8f8f434247cb72cd901027b6a0d3e
KrollBio <kylekroll@outlook.com>
2019-05-03 18:55:31 -0400

    Updates

--- R/launchApp.R
+++ R/launchApp.R
@@ -1,5 +1,12 @@
+#' Launch CytoDRAV.
+#'
+#' Function requires no parameters but accepts optional parameters
+#' that can be passed in \code{\link[shiny]{runApp}}, such as \code{launch.browser}
+#' @examples
+#' launch_application()
+#' launch_application(launch.browser=TRUE)
 #' @export
-launch_application <- function(x, ...)
+launch_application <- function(...)
 {
   shiny::runApp(appDir = system.file("application", package = 'CytoDRAV'),
                 ...)
