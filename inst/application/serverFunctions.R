8a562c28d145377977d41e53bf7391a8e2cea091
Kyle Kroll <kkroll1@bidmc.harvard.edu>
2019-08-07 09:46:34 -0400

    Reboot of CytoDRAV.

--- inst/application/serverFunctions.R
+++ inst/application/serverFunctions.R
@@ -1,130 +1,49 @@
-## FCS Loading
-loadFCS <- function(fcsFiles, doTransform) {
-  fcs_dataframes <- c()
-  withProgress(message="Reading FCS files...", value=0, min=0, max=length(fcsFiles[,1]), {
-    for (i in 1:length(fcsFiles[,1])) {
-      single_fcs_raw <- flowCore::read.FCS(fcsFiles[i, "datapath"])
+# About ----
+# Project: CytoDRAV
+# Author: Kyle Kroll
+# Version: 2.0
+# This file holds all generic functions that are used in the program
+# Offloaded these functions to create a distinction between shiny code and behind the scenes R code
 
-      # Biexponential transformation. Uses a linear transformation for data around 0 and log transformation
-      # for larger values
-      if (isTRUE(doTransform)) {
-        m = 4.5
-        t = 262144
-        w = 1.5
-        lgcl <- flowCore::logicleTransform( w = w, t= t, m = m)
-        single_fcs_transformed <- flowCore::transform(single_fcs_raw,
-                                                      flowCore::transformList(paste(single_fcs_raw@parameters@data$name), lgcl))
-
-        single_fcs_df <- data.frame(single_fcs_transformed@exprs)
-      } else {
-        single_fcs_df <- data.frame(single_fcs_raw@exprs)
-      }
-
-
-      # Get the $PnS from the FCS file. If $PnS is NA (FSC, SSC, Time, Event) then use $PnN
-      column_names <- c()
-      for (j in 1:ncol(single_fcs_df)) {
-        if (is.na(single_fcs_raw@parameters@data$desc[[j]])) {
-          column_names <- c(column_names, single_fcs_raw@parameters@data$name[[j]])
-        } else {
-          column_names <- c(column_names, single_fcs_raw@parameters@data$desc[[j]])
-        }
-      }
-      colnames(single_fcs_df) <- column_names
-
-      # Use input filename as sample keyword in the dataframe
-      name <- strsplit(fcsFiles[i, "name"], "[.]")[[1]][1]
-      name <- gsub(" ", "_", name)
-      name <- gsub("\\+", ".POS", name)
-      name <- gsub("\\-", ".NEG", name)
-      single_fcs_df$Sample <- as.character(rep(name, nrow(single_fcs_df)))
-
-      # Add the single FCS data to a list of dataframes
-      fcs_dataframes[[i]] <- single_fcs_df
-      incProgress(1)
-    }
-  })
-
-  # Flow data is gated sequentially. So if you include your 'Live' population, that will also include all your
-  # other populations. This increases the number of data in you analysis, which makes it slower. This portion
-  # sorts the input files from smallest to larget and removes duplicates. R works in such a way that the first event
-  # it finds and removes subsequent matches.
-
-  # Sort the list of dataframes by number of rows and join them into a single dataframe
-  fcs_dataframes = fcs_dataframes[order(sapply(fcs_dataframes,nrow),decreasing = F)]
-  exprsData <- data.frame()
-  for (i in 1:length(fcs_dataframes)) exprsData <- rbind(exprsData, fcs_dataframes[[i]])
-
-  # Remove duplicates and shuffle dataset
-  exprsData <- exprsData[!duplicated(exprsData[,!colnames(exprsData)%in%c("Sample")]),]
-  exprsData <- exprsData[sample(1:nrow(exprsData)),]
-  return(exprsData)
-}
-
-## PLOTTING FUNCTIONS
-create_plot <- function(dataToPlot, marker="Sample", dotsize, dotalpha, sampleColor="black", show_legend,show_axis_labels, show_title) {
-  data <- as.data.frame(dataToPlot)
-  plot <- ggplot2::ggplot(data) + ggplot2::aes(x=data[,"tSNEX"], y=data[,"tSNEY"])
-
-  if (marker == "Sample") {
-
-    plot <- plot + ggplot2::scale_colour_manual(values=sampleColor) +
-      ggplot2::geom_point(ggplot2::aes(color=data[,marker]), size=dotsize, alpha=dotalpha) +
-      ggplot2::labs(x="bh-SNE1", y="bh-SNE2", color="") +
-      ggplot2::ggtitle("Sample") +
-      ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(size=3), nrow = 1))
-
-  } else if(marker == "Density") {
-    df <- data
-    x <- densCols(df$tSNEX,df$tSNEY, colramp=colorRampPalette(c("black", "white")))
-    df$dens <- col2rgb(x)[1,] + 1L
-    cols <-  colorRampPalette(c("#000099", "#00FEFF", "#45FE4F","#FCFF00", "#FF9400", "#FF3100"))(256)
-    df$col <- cols[df$dens]
-    plot <- ggplot2::ggplot(df[order(df$dens),], ggplot2::aes(tSNEX, tSNEY, color = dens)) +
-      ggplot2::geom_point(size=dotsize, alpha=dotalpha) +
-      ggplot2::scale_color_gradientn(colors = unique(df[order(df$dens),'col']),
-                                     breaks = c(min(df$dens), median(df$dens), max(df$dens)),
-                                     labels = c("Low", "", "High")) +
-      ggplot2::labs(x="bh-SNE1", y="bh-SNE2", color="") +
-      ggplot2::ggtitle("Density") +
-      ggplot2::guides(colour = ggplot2::guide_colourbar(title = ggplot2::waiver(), barwidth = 25))
-
-  } else {
-
-    plot <- plot +
-      ggplot2::geom_point(ggplot2::aes(color=normData(data[, marker])), size=dotsize, alpha=dotalpha) +
-      ggplot2::scale_color_gradientn(colors=c("blue", "cyan", "yellow", "red" ),
-                                     breaks=c(min(normData(data[, marker])),
-                                              median(normData(data[, marker])),
-                                              max(normData(data[, marker]))),
-                                     labels = c("Low", "", "High")) +
-      ggplot2::labs(x="bh-SNE1", y="bh-SNE2", color="") +
-      ggplot2::ggtitle(marker) +
-      ggplot2::guides(colour = ggplot2::guide_colourbar(title = ggplot2::waiver(), barwidth = 25))
-
-
-  }
-  plot <- plot +
-    ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_blank(),
-                   legend.text = ggplot2::element_text(size=12, face="bold"),
-                   legend.title = ggplot2::element_text(size=16, face="bold"),
-                   panel.background = ggplot2::element_blank(),
-                   axis.line = ggplot2::element_line(colour = "black"),
-                   axis.title = ggplot2::element_text(size=18, face="bold"),
-                   plot.title = ggplot2::element_text(size=18, face = "bold"),
-                   legend.position = "bottom",
-                   legend.direction = "horizontal")
-  if (!show_legend) plot <- plot + ggplot2::theme(legend.position="none")
-  if (!show_title) plot <- plot + ggplot2::theme(plot.title = ggplot2::element_blank())
-  if (!show_axis_labels) plot <- plot + ggplot2::theme(axis.title = ggplot2::element_blank())
-  return(plot)
-}
-
-# Sets outliers equal to the 1st and 99th percentile
+# Outlier correction for plots ----
 normData <- function(x){
   quantiles <- quantile( x, c(.01, .99 ) )
   x[ x < quantiles[1] ] <- quantiles[1]
   x[ x > quantiles[2] ] <- quantiles[2]
   x
 }
+# Plotting function ----
+create_plot <- function(dataToPlot, marker, dotsize, dotalpha, show_legend,show_axis_labels, show_title) {
+    data <- as.data.frame(dataToPlot)
+    plot <- ggplot2::ggplot(data) + 
+        ggplot2::aes(x=data[, "tSNEX"], y=data[, "tSNEY"]) +
+        ggplot2::geom_point(ggplot2::aes(color=data[, marker]),size=dotsize, alpha=dotalpha) +
+        ggplot2::labs(x=x, y=y, color="") +
+        ggplot2::ggtitle("Population") +
+        ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(size=3), nrow = 1)) +
+        ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
+                       panel.grid.minor = ggplot2::element_blank(),
+                       legend.text = ggplot2::element_text(size=12, face="bold"),
+                       legend.title = ggplot2::element_text(size=16, face="bold"),
+                       panel.background = ggplot2::element_blank(),
+                       axis.line = ggplot2::element_line(colour = "black"),
+                       axis.title = ggplot2::element_text(size=18, face="bold"),
+                       plot.title = ggplot2::element_text(size=18, face = "bold"),
+                       legend.position = "bottom",
+                       legend.direction = "horizontal")
+    if (!show_legend) plot <- plot + ggplot2::theme(legend.position="none")
+    if (!show_title) plot <- plot + ggplot2::theme(plot.title = ggplot2::element_blank())
+    if (!show_axis_labels) plot <- plot + ggplot2::theme(axis.title = ggplot2::element_blank())
+    return(plot)
+}
+
+# Z-score transform ----
+z_transform <- function(x) {
+    zVar <- (x - mean(x)) / sd(x)
+    zVar
+}
+
+# Loadng data from flowWorkspace ----
+load_nodes <- function(gating_set, populations) {}
+# Loading ACS files ----
+extract_acs <- function(file, dir) {}
--- inst/application/ui.R
+++ inst/application/ui.R
@@ -1,120 +1,73 @@
-# UI generation for CytoDRAV
+# About ----
+# Application: CytoDRAV
+# Version: 2.0
+# Changelog: Complete overhaul of CytoDRAV.
+# Read more at https://github.com/ReevesLab/CytoDRAV/Reboot
 
-fluidPage(
 
-  shinyjs::useShinyjs(),
+{fluidPage(
+    titlePanel("CytoDRAV 2.0"),
+    # Sidebar ----
+        sidebarLayout(
+            sidebarPanel(
+                fileInput("wsp_upload", "Select ACS File", multiple=FALSE),
+                    div(style="margin-top:-1em",
+                    actionButton("load_acs", "Load ACS")
+                    ),
+                hr(),
+                uiOutput(outputId = "plot_style"),
+                hr(),
+                div(id="outDiv", verbatimTextOutput("consoletext")),
+                    tags$head(tags$style("#outDiv{overflow-y:scroll; max-height: 100px;}")),
+                    tags$script(
+                        '
+                        Shiny.addCustomMessageHandler("scrollCallback",
+                        function(color) {
+                        var objDiv = document.getElementById("outDiv");
+                        objDiv.scrollTop = objDiv.scrollHeight;
+                        }
+                        );'
+                    )
+            ),
+            mainPanel(
+                tabsetPanel(id = "main",
+                            tabPanel("Plot", value = "plotting",
+                                     shinyWidgets::dropdownButton(
+                                         fluidRow(
+                                             column(width=6,
+                                                    sliderInput(inputId="size", "Dot Size", min=0.1, max=5, value=1),
+                                                    sliderInput(inputId="alpha", "Dot alpha", min=0.1, max=1, value=1),
+                                                    checkboxInput("show_legend", "Legend", value=TRUE),
+                                                    checkboxInput("show_axis_labels", "Axis Labels", value=TRUE),
+                                                    checkboxInput("show_title", "Title", value=TRUE),
+                                                    downloadButton("export", "Save plot"),
+                                                    downloadButton("saveRDF", "Save data")),
+                                             column(width=6, uiOutput("overlay"))),
+                                         circle = FALSE, status = "danger",
+                                         icon = icon("sliders-h"), width = "300px",
 
-  titlePanel("CytoDRAV - Dimensionality Reduction and Visualization for Flow Cytometry"),
+                                         tooltip = shinyWidgets::tooltipOptions(title = "Plot Settings and Download")),
+                                     plotOutput("plot", width="100%", height=750)
+                            ),
+                            tabPanel("Settings",
+                                     id = "settings",
+                                     tabsetPanel(id = "settings_tabs",
+                                                 tabPanel("Gating", value = "gating",
+                                                          plotOutput(outputId = "gh_plot")),
+                                                 tabPanel("Population", value = "population",
+                                                          textOutput(outputId = "label"),
+                                                          uiOutput(outputId = "gated_pop_selection"),
+                                                          textOutput(output = "num_events")),
+                                                 tabPanel("Parameters", value = "parameters",
+                                                          fluidRow(
+                                                              column(2, uiOutput(outputId = "sel_params")),
+                                                              column(10, uiOutput(outputId = "params_alg"))
+                                                          )
+                                                 )
+                                     )
+                            )
+                )
+            )
+        )
 
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file1", "Choose FCS File", multiple=TRUE, accept=c("text/fcs", ".fcs")),
-      checkboxInput("transform", "Transform Data", value=FALSE),
-      div(style="margin-top:-1em", actionButton("load_fcs_files", "Load FCS")),
-      hr(),
-      fileInput("filep", "Choose Previous Session", multiple=FALSE, accept=c("text/rda", ".rda")),
-      div(style="margin-top:-1em", actionButton("load_previous_file", "Load previous session")),
-      hr(),
-      fluidRow(
-        actionButton("run_tsne", "Run bh-SNE"),
-        actionButton("plot_tsne", "Plot bh-SNE")
-      ),
-      div(id="outDiv", verbatimTextOutput("consoletext")),
-      tags$head(tags$style("#outDiv{overflow-y:scroll; max-height: 100px;}")),
-      tags$script(
-        '
-        Shiny.addCustomMessageHandler("scrollCallback",
-        function(color) {
-        var objDiv = document.getElementById("outDiv");
-        objDiv.scrollTop = objDiv.scrollHeight;
-        }
-        );'
-    )
-    ),
-
-    # All the div calls are so the buttons would be on the same row.
-    # I should look into fluidRow to cut down the clutter here.
-    mainPanel(
-      tabsetPanel(id="apptabs",
-
-                  tabPanel(title="Main", value = "maintab",
-                           shinyWidgets::dropdownButton(
-                             fluidRow(
-                             column(width=6,
-                             sliderInput(inputId="size", "Dot Size", min=0.1, max=5, value=1),
-                             sliderInput(inputId="alpha", "Dot alpha", min=0.1, max=1, value=1),
-                             checkboxInput("show_legend", "Legend",
-                                           value=TRUE),
-                             checkboxInput("show_axis_labels", "Axis Labels",
-                                           value=TRUE),
-                             checkboxInput("show_title", "Title",
-                                           value=TRUE),
-                             downloadButton("export", "Save plot"),
-                             downloadButton("saveRDF", "Save data")),
-                             column(width=6,
-                                    uiOutput("overlay"),
-                                    uiOutput("color_selector"))),
-                             circle = FALSE, status = "danger",
-                             icon = icon("sliders-h"), width = "300px",
-
-                             tooltip = shinyWidgets::tooltipOptions(title = "Plot Settings and Download")),
-                          plotOutput("plot", width="100%", height=750)
-                          ),
-
-                  tabPanel(title="Parameters", value = "paramtab",
-                           fluidRow(
-                           column(2,
-                                  uiOutput("maker_select_tsne"),
-                                  uiOutput("ncell_slider")
-                                  ),
-                           column(2,
-                                  h4("bh-SNE Parameters"),
-                                  checkboxInput("initpca", "Initial PCA Step", TRUE),
-                                  numericInput("perp", "Perplexity", min=0, value=20),
-                                  numericInput("ndims", "Output dimensions", min=2, value=2),
-                                  numericInput("theta", "Theta", min=0.5, value=0.5),
-                                  numericInput("eta", "Learning rate", min=50, value=200),
-                                  numericInput("iter", "Iterations", min = 500, max = 5000, value = 1000),
-                                  selectInput("n_threads", "Number of Threads",
-                                              choices = seq(1:(unname(future::availableCores())-1)),
-                                              selected = 1)
-                                  ),
-                           column(8,
-                                  p("Settings shown are default. Settings explained:"),
-                                  p("Initial PCA Step - Should PCA be computed first to initially reduce dimensions to variables that explain the most variance."),
-                                  p("Output dimensions - How many dimensions should the output have. Currently only display of 2 dimensions is supported."),
-                                  p("Theta - Speed/accuracy trade-off (increase for less accuracy), set to 0.0 for exact tSNE "),
-                                  p("Learning rate - How much variation is calculated during each iteration"),
-                                  p("Iterations - How many iterations to run before ending the calculations. Max 5000")
-                                  )
-                           )),
-
-                  tabPanel("Export",
-                           fluidRow(
-                             h4("Plot Export Settings"),
-                                    column(2, uiOutput("exportmarkers")),
-                                    column(2, selectInput("exportFormat", "Filetype:", choices = c("pdf", "png", "jpg"),
-                                                          selected = "pdf")),
-                                    column(4,
-                                           div(style="display: inline-block;vertical-align:top; width: 100px;",
-                                               actionButton("generate_plots", "Generate")),
-                                           div(style="display: inline-block;vertical-align:top; width: 100px;",
-                                               downloadButton("download_plots", "Download"))
-                                    )
-                           )
-                          ),
-
-                  tabPanel("About",
-                           h4("About CytoDRAV"),
-                           p("CytoDRAV was written by Kyle Kroll at the Reeves Lab, Center for Virology and Vaccine Research at
-                             Beth Israel Deaconess Medical Center/Harvard Medical School",
-                             tags$br(),
-                             tags$br(),
-                             "CytoDRAV was written because our lab had a need for an easy-to-use tool for the computation and
-                             visualization of tSNE."), br(),
-                           htmlOutput("analysis_params")
-                          )
-      )
-    )
-  )
-)
+)}
